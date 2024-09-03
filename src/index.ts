1ef2a56dff1d77d43e317cc2cdada5042e1fadf3
strelka <pedrocalizto9@gmail.com>
2024-09-03 10:55:55 -0300

    ping

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,20 @@
 import {config} from 'dotenv'
 import Express from 'express'
 
-import UsuarioRouter from './routes/user'
-import Log from './util/log'
+import UsuarioRouter from './routes/user.js'
+import Log from './util/log.js'
 
 config()
-Log.Clear()
+Log.Text("Start", "")
 
 const app = Express()
 app.use(Express.json())
 
-app.use(Express.static("src/public") )
+app.use("/", Express.static("../public") )
 app.use("/api/v1/usuario", UsuarioRouter )
+
+app.get("/ping", (req, res) =>
+	res.end("Hello, World!!"))
 
 const PORT = process.env['SERVER_PORT'] || 3000
 app.listen(PORT, () =>
