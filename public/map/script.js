a461a6d0a372ae75672816c408f336734617ff31
DestinyFrog <107517847+DestinyFrog@users.noreply.github.com>
2024-09-13 20:06:35 -0300

    frontend: map functionality

--- public/map/script.js
+++ public/map/script.js
@@ -1,15 +1,31 @@
 
+var map = L.map('map')
+
+var layer = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {  attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community' })
+layer.addTo(map)
+
+navigator.geolocation.getCurrentPosition((position) => {
+	map.setView([position.coords.latitude, position.coords.longitude], 14)
+	L.marker([position.coords.latitude, position.coords.longitude])
+		.addTo(map)
+})
+
 fetch("/api/v1/map")
 .then(resp => {
 
-	switch(resp.status) {
-		case 200:
-			resp.json()
-			.then((data) =>
-				console.log(data)
-			)
-			.catch(err => { throw err })
-			break
+	if (resp.status == 200) {
+		resp.json()
+		.then((data) =>
+			data.forEach(cidade => {
+				cidade.bairros.forEach(({nome, xpos, ypos}) => {
+					L.tooltip()
+						.setLatLng( [xpos, ypos] )
+						.setContent(nome)
+						.addTo(map)
+				})
+			})
+		)
+		.catch(err => { throw err })
 	}
 
 })
